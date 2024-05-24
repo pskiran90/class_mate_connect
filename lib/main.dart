c8bc17b6f59f4620ddd569c60413f46b55a22fec
kiranpsvidealpha <116801237+pskiran90@users.noreply.github.com>
2024-05-24 15:35:45 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+import 'mainApp/constants/app_constants.dart';
+import 'mainApp/screens/dashboard/dashboard_screen.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: AppConstants.appTitle,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const DashboardScreen(),
+    );
+  }
+}
---
+++ lib/mainApp/screens/dashboard/dashboard_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class DashboardScreen extends StatelessWidget {
+  const DashboardScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Column(
+          children: [Card()],
+        ),
+      ),
+    );
+  }
+}
